1e88753d9b586cbec98103fd7be60d2bf3a8eefc
Rohit Kumar <rohit9075@gmail.com>
2018-06-26 12:40:13 +0530

    workingwithString

---
+++ src/WorkingWthString.java
@@ -0,0 +1,11 @@
+public class WorkingWthString {
+
+    public static void main(String[] args) {
+        String name = "rohit ";
+        String lastName = "kumar";
+        String contact = name  +lastName;
+        System.out.println(contact);
+    }
+
+
+}
