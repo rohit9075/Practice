45daa9087a55132107a3327d76a86ed70a412889
Rohit Kumar <rohit9075@gmail.com>
2018-09-01 22:41:54 +0530

    counting digits in number

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int digit = 0;
+
+        System.out.println("Please enter the number to find the digit : ");
+        int number = input.nextInt();
+
+        while (number != 0){
+
+            digit++;
+            number = number/10;
+        }
+
+        System.out.println(digit);
+    }
+
+}
+
