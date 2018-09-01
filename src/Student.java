aac7d3cdad23a27b402952ac3ec84dadae2d9d09
Rohit Kumar <rohit9075@gmail.com>
2018-09-01 22:31:46 +0530

    immutable class

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,11 @@
+public final class Student {
 
-
-public class Student {
-
-    public static void main(String[] args) {
-        System.out.println("Rohit kumar");
+    public  static void print(){
+        final int a = 10;
+        System.out.println(a);
     }
 
+    public static void main(String[] args) {
+      Student.print();
+    }
 }
---
+++ src/ThreadTest.java
@@ -0,0 +1,6 @@
+public class ThreadTest {
+
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+    }
+}
